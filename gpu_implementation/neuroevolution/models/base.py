35cb50d52307d2932d82d6a731b1ad24e13f5213
Deyan Dyankov <deyan@dyankov.name>
2019-06-19 22:35:45 +0100

    Use the top 100 performing offspring for the new generation

--- gpu_implementation/neuroevolution/models/base.py
+++ gpu_implementation/neuroevolution/models/base.py
@@ -115,6 +115,10 @@ class BaseModel(object):
             self.indices = indices
             self.graph = tf.get_default_graph()
             a = self._make_net(x, num_actions)
+
+            #import ipdb
+            #ipdb.set_trace()
+
             return tf.reshape(a, (-1, num_actions))
 
     def _make_net(self, x, num_actions):
